986c37f5853c94acdb5e19854cab8a1a56a34d7a
Carla Machado <carlamrosa438@gmail.com>
2022-03-22 23:40:28 -0300

    updates css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import "./App.css";
 import ToUppercase from "./components/uppercase";
 
 function App() {
-  return <ToUppercase />;
+  return (
+  <>
+  <ToUppercase />
+  <ToUppercase />
+  </>
+  )
 }
 
 export default App;
